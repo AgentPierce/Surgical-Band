fea2757ab27b9c032d1066addabae8061d5a9266
AgentPierce <greenbebrt@gmail.com>
2015-10-24 15:39:46 -0700

    FinaL UI

--- Surgical Band/CheckList.xaml.cs
+++ Surgical Band/CheckList.xaml.cs
@@ -48,8 +48,13 @@ namespace Surgical_Band
 
         private void CheckList_Loaded(object sender, RoutedEventArgs e)
         {
-                talkBack(questionList[0]);
-                listenIn();
+            question1.Text = questionList[0];
+            question2.Text = questionList[1];
+            question3.Text = questionList[2];
+            question4.Text = questionList[3];
+            question5.Text = questionList[4];
+            talkBack(questionList[0]);
+            listenIn();
         }
 
         private async void listenIn()
@@ -76,24 +81,29 @@ namespace Surgical_Band
                 if (queCounter == 1)
                 {
                     await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => talkBack(questionList[queCounter]));
+                    await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => tick1.Visibility = Visibility.Visible);
                     queCounter++;
                 }
                 else if (queCounter == 2)
                 {
                     await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => talkBack(questionList[queCounter]));
+                    await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => tick2.Visibility = Visibility.Visible);
                     queCounter++;
                 }
                 else if (queCounter == 3)
                 {
                     await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => talkBack(questionList[queCounter]));
+                    await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => tick3.Visibility = Visibility.Visible);
                     queCounter++;
                 }
                 else if (queCounter == 4)
                 {
                     await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => talkBack(questionList[queCounter]));
+                    await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => tick4.Visibility = Visibility.Visible);
                     queCounter++;
                 }
                 else if (queCounter == questionList.Count()) {
+                    await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => tick5.Visibility = Visibility.Visible);
                     await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () => talkBack("Sign in checklist complete, you can track equipment now."));
                     await sender.StopAsync();
                 }
